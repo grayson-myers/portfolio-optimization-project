4269e642e56d63bb1c6b5655953833bf38fcacea
Grayson Myers <graymyers10@gmail.com>
2026-04-20 13:21:42 -0600

    Update trajectory simulation

--- trajectory_driver.m
+++ trajectory_driver.m
@@ -1,13 +1,14 @@
-mu = 0.05; % average growth rate of asset
-sigma = 5 * (1./linspace(0.5,3,100) + 1./linspace(5,0.4,100)); % volatility of growth rate (vector equal in size to time vector)
+n = 1000; % number of points 
+mu = 0.1; % average growth rate of asset
+sigma = 0.0 * (1./linspace(0.5,3,n) + 1./linspace(5,0.4,n)); % volatility of growth rate (vector equal in size to time vector)
 Xzero = 1; % starting value for X, in this case our initial wealth
-t = linspace(0,5,100); % vector of timestamps to calculate discretized brownian
+t = linspace(0,1,n); % vector of timestamps to calculate discretized brownian
 M = 100; % number of simulations to perform
 
 [t,X] = trajectory_simulation(mu,sigma,Xzero,t,M); %X matrix will be [M x length(t)]
 
 % visualization
-plot(t, X);
+plot(t, (X)');
 xlabel('Time');
 ylabel('Trajectory');
 title('Trajectory Simulation');
